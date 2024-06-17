5cd8ce8ea9341419b0fee0926c775f3cf1bd1730
brylee10 <bryanlee99@gmail.com>
2024-06-16 23:28:55 -0400

    Add default page for server

--- rust/app/src/lib.rs
+++ rust/app/src/lib.rs
@@ -1,6 +1,13 @@
+use warp::Filter;
+
 pub mod execute;
 pub mod messages;
 
 pub mod sessions;
 pub mod users;
 pub mod websocket;
+
+/// Returns a "Server connected" HTML message at the root path
+pub fn default_page() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path::end().map(|| warp::reply::html("Corust server connected"))
+}
